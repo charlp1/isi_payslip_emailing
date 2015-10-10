8bb780ca6ff2b47566280f992e9ba7ec669e8de1
Pauline Anasco <panasco.eeweb@gmail.com>
2015-10-10 17:08:13 +0800

    no message

--- isi_payslip_email/payslip/urls.py
+++ isi_payslip_email/payslip/urls.py
@@ -5,7 +5,7 @@ from .views import Home, EmployeeView, PayslipSendView, PayslipUploadView
 
 urlpatterns = patterns("",
                        url(r'^$', Home.as_view(), name='home'),
-                       url(r'^employee/$', EmployeeView.as_view(), name='employee'),
+                       url(r'employee/$', EmployeeView.as_view(), name='employee'),
                        url(r"send/?$", PayslipSendView.as_view(), name="send-payslip"),
                        url(r"upload/?$", PayslipUploadView.as_view(), name="upload-payslip")
                        )

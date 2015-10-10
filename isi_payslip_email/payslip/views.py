cd523b3a34644e67eb8af3fb75f160745b30c9c7
Neil Espera <nespera.eeweb@gmail.com>
2015-10-10 15:42:01 +0800

    upload

--- isi_payslip_email/payslip/views.py
+++ isi_payslip_email/payslip/views.py
@@ -1,11 +1,18 @@
 from __future__ import absolute_import
 
+
+from os.path import join
+from datetime import date, timedelta
 from django.core.exceptions import ObjectDoesNotExist
 from django.views.generic import TemplateView, View
 from django.views.decorators.csrf import csrf_exempt
 
+from django.core.files.base import ContentFile
+from django.core.files.storage import default_storage
 from django.shortcuts import redirect
 from django.views.generic import FormView, TemplateView
+from django.conf import settings
+
 
 from .forms import SendPayslipForm
 from .models import Employee
@@ -54,4 +61,33 @@ class PayslipUploadView(TemplateView):
     template_name = 'payslip/upload.html'
 
     def post(self, request):
-        pass
+        data = request.FILES['files[]']
+
+        date_today = date.today()
+        today = date_today.strftime('%d')
+
+        f_day = get_first_day(date_today)
+        f = 16 if today > 15 else f_day.strftime('%d')
+
+        l_day = get_last_day(date_today)
+        pay_folder = "{0}-{1}-{2}".format(date_today.strftime('%Y-%m'), f, l_day.strftime('%d'))
+        path = default_storage.save(join(settings.MEDIA_ROOT, pay_folder, data.name), ContentFile(data.read()))
+
+
+        response_kwargs = {
+            "request": self.request,
+            "template": self.template_name,
+            "context": self.get_context_data()
+        }
+        return self.response_class(**response_kwargs)
+
+
+def get_first_day(dt, d_years=0, d_months=0):
+    # d_years, d_months are "deltas" to apply to dt
+    y, m = dt.year + d_years, dt.month + d_months
+    a, m = divmod(m-1, 12)
+    return date(y+a, m+1, 1)
+
+
+def get_last_day(dt):
+    return get_first_day(dt, 0, 1) + timedelta(-1)

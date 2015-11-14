1ac077d6f21e5232e757238699015086fdfefa39
Charl Ishii <yukicharlishii@gmail.com>
2015-11-14 12:47:29 +0800

    Update upload payslip

--- isi_payslip_email/payslip/static/email.jsx
+++ isi_payslip_email/payslip/static/email.jsx
@@ -1,29 +1,70 @@
 // Table:
 // name, email, active, send mail
 
+var EmployeeInfo = React.createClass({
+
+    "handleSelect": function( e ) {
+
+        this.props.onClickSelectEmployee( this.props.info );
+
+    },
+    "render": function() {
+
+        var index = this.props.index;
+        var info = this.props.info;
+
+        return (
+            <tr key={ index + info.name }>
+                <td>
+                    <input type="checkbox"
+                        checked={ info.selected }
+                        onChange={ this.handleSelect } />
+                </td>
+                <td>{ info.send_status }</td>
+                <td>{ info.name }</td>
+                <td>{ info.email }</td>
+                <td>{ info.active ? "active" : "inactive" }</td>
+                <td>{ info.send_email ? "yes" : "no" }</td>
+            </tr>
+        );
+
+    }
+
+});
+
 // Employee List
 var EmployeeList = React.createClass({
 
+    "handleSelectAll": function( e ) {
+
+        this.props.onClickSelectAll();
+
+    },
     "render": function() {
 
+        var props = this.props;
         var employeeInfo = function( info, index ) {
 
             return (
-                <tr key={ index + info.name }>
-                    <td>{ info.name }</td>
-                    <td>{ info.email }</td>
-                    <td>{ info.active ? "active" : "inactive" }</td>
-                    <td>{ info.send_email ? "yes" : "no" }</td>
-                </tr>
+                <EmployeeInfo index={ index }
+                    info={ info }
+                    onClickSelectEmployee={ props.onClickSelectEmployee } />
             );
 
         };
-        console.log( "items:", this.props.items );
+        console.log( "checkbox select all:", this.props.selectAll );
+        console.log( "checkbox employees:", this.props.items );
 
         return (
             <table className="table table-striped table-hover table-bordered">
                 <thead>
                     <tr>
+                        <th>
+                            <input type="checkbox"
+                                checked={ this.props.selectAll }
+                                onChange={ this.handleSelectAll } />
+                        </th>
+                        <th>Send Status</th>
                         <th>Name</th>
                         <th>Email</th>
                         <th>Status</th>
@@ -43,6 +84,12 @@ var EmployeeList = React.createClass({
 var EmailApp = React.createClass( {
 
     "componentWillMount": function() {
+
+        amplify.request.define( "get_employee_list", "ajax", {
+            "url": "/payslip/employee/",
+            "dataType": "json",
+            "type": "GET"
+        });
 
         amplify.request.define( "send-email", "ajax", {
             "url": "/payslip/employee/",
@@ -69,33 +116,50 @@ var EmailApp = React.createClass( {
     },
     "getEmployeeList": function() {
 
+        // Send status: ready, active, success, error
         var employees = [
                 {
                     "name": "employee1",
                     "email": "test1@gmail.com",
                     "active": true,
-                    "send_email": true
+                    "send_email": true,
+                    "send_status": "ready",
+                    "selected": true
                 },
                 {
                     "name": "employee2",
                     "email": "test2@gmail.com",
                     "active": true,
-                    "send_email": true
+                    "send_email": true,
+                    "send_status": "ready",
+                    "selected": true
                 },
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true},
-                {"name": "employee2", "email": "test2@gmail.com", "active": true, "send_email": true}
+                {"name": "employee3", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee4", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee5", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee6", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee7", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee8", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee9", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee10", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee11", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee12", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true},
+                {"name": "employee13", "email": "test2@gmail.com", "active": true, "send_email": true, "send_status": "ready", "selected": true}
             ];
         var defer = $.Deferred();
+
+        // TODO
+        amplify.request({
+            resourceId: "get_employee_list",
+            data: {},
+            success: function( data, status ) {
+
+            },
+            error: function( error, status ) {
+
+            }
+        });
+
         defer.resolve( employees );
         return defer.promise();
 
@@ -110,8 +174,47 @@ var EmailApp = React.createClass( {
     "getInitialState": function() {
 
         return {
-            "employees": []
+            "employees": [],
+            "selectAll": true
         };
+
+    },
+    "sendEmailRequest": function( employee ) {
+
+        var defer = $.Deferred();
+
+        // TODO
+        //amplify.request({
+        //    resourceId: "send_email",
+        //    data: {},
+        //    success: function( data, status ) {
+        //
+        //    },
+        //    error: function( error, status ) {
+        //
+        //    }
+        //});
+
+        setTimeout( function() {
+            defer.resolve( employee );
+        }, 3000 );
+
+        return defer.promise();
+
+    },
+    "setEmployeeSendStatus": function( name, status ) {
+
+        console.log( "set employee status:", name, status );
+        var employees = _.map( this.state.employees, function( employee ) {
+            if ( employee.name === name ) {
+                employee.send_status = status;
+            }
+            return employee;
+        });
+
+        this.setState({
+            "employees": employees
+        });
 
     },
     "onClickSendEmail": function( e ) {
@@ -119,15 +222,75 @@ var EmailApp = React.createClass( {
         e.preventDefault();
 
         alert( "send email" );
+
         // get list of employees checked
         // send request asynchronously
+        var self = this;
+        _.each( this.state.employees, function( employee ) {
+
+            self.sendEmailRequest( employee ).then(
+                function onSuccess() {
+                    self.setEmployeeSendStatus( employee.name, "success" );
+                },
+                function onError() {
+                    self.setEmployeeSendStatus( employee.name, "error" );
+                }
+            );
+
+        });
+
+    },
+    "onClickSelectAll": function() {
+
+        var prevState = this.state;
+        var newSelectAll = !prevState.selectAll;
+
+        var newEmployees = _.map( prevState.employees, function( employee ) {
+            employee.selected = newSelectAll;
+            return employee;
+        });
+
+        this.setState({
+            "employees": newEmployees,
+            "selectAll": newSelectAll
+        });
+
+    },
+    "onClickSelectEmployee": function( employee ) {
+
+        // Update employee select checkbox
+        var employees = _.map( this.state.employees, function( currentEmployee ) {
+            if ( currentEmployee.name === employee.name ) {
+                currentEmployee.selected = !currentEmployee.selected;
+            }
+            return currentEmployee;
+        });
+
+        // Update select all employees checkbox
+        var allEmployeesSelected = true;
+        _.each( employees, function( currentEmployee ) {
+            if ( !currentEmployee.selected ) {
+                allEmployeesSelected = false;
+            }
+        });
+
+        this.setState({
+            "employees": employees,
+            "selectAll": allEmployeesSelected
+        });
+
+        console.log( "select employee:", employee );
+
     },
     "render": function() {
 
         return (
             <div>
                 <div id="send-email-table">
-                    <EmployeeList items={ this.state.employees } />
+                    <EmployeeList items={ this.state.employees }
+                        selectAll={ this.state.selectAll }
+                        onClickSelectAll={ this.onClickSelectAll }
+                        onClickSelectEmployee={ this.onClickSelectEmployee } />
                 </div>
                 <div id="send-email-button">
                     <button className="btn btn-success flex-auto"
--- isi_payslip_email/payslip/static/upload.js
+++ isi_payslip_email/payslip/static/upload.js
@@ -1,6 +1,7 @@
 $(function () {
 
-    var url = "payslip/upload";
+    var url = "/payslip/upload/";
+
     var uploadAlert = $( "#upload-alert" );
     var progressBar = $( "#progress .progress-bar" );
     var uploadTotalFiles = $( "#upload-total-files" );
@@ -19,9 +20,165 @@ $(function () {
         "fail": 0
     };
 
+    var failedToUploadDate = "";
+    var missingPayslipButton = $( "#missing-payslip-button" );
+    var missingPayslipDate = $( "#missing-payslip-date" );
+    var missingPayslipListContainer = $( "#missing-payslip-list" );
+    var missingPayslipList = $( ".result", "#missing-payslip-list" );
+
+    /**
+     * Request missing payslips
+     */
+    // Disable get missing payslip button
+    missingPayslipButton.attr( "disabled", "disabled" );
+
+    // Hide missing payslip date and list
+    missingPayslipDate.hide();
+    missingPayslipListContainer.hide();
+
+    amplify.request.define( "get_missing_payslips", "ajax", {
+        "url": "/payslip/api/missing_employee/",
+        "dataType": "json",
+        "type": "GET"
+    });
+
+    var requestMissingPayslip = function() {
+
+        var defer = $.Deferred();
+
+        amplify.request({
+            resourceId: "get_missing_payslips",
+            data: {
+                "payslip": failedToUploadDate
+            },
+            success: function( data, status ) {
+                defer.resolve( data, status );
+            },
+            error: function( error, status ) {
+                defer.reject( error, status );
+            }
+        });
+
+        return defer.promise();
+    };
+
+    var onGetMissingPayslips = function() {
+
+        requestMissingPayslip().then(
+            function onSuccess( response, status ) {
+                var payslipList = _.map( response.data, function( payslip ) {
+                    return '<li class="list-group-item">' + payslip + "</li>";
+                });
+                missingPayslipListContainer.show();
+                missingPayslipList.html( payslipList.join("") );
+            },
+            function onError( error, status ) {
+                missingPayslipListContainer.show();
+                missingPayslipList.html( error && error.toString() );
+            }
+        );
+
+    };
+
+    missingPayslipButton.on( "click", onGetMissingPayslips );
+
+    /**
+     * Upload payslip
+     */
+    // Hide upload status messages, and list
     uploadAlert.hide();
     uploadSuccessContainer.hide();
     uploadErrorContainer.hide();
+
+    var updateProgressBar = function( progress ) {
+
+        progressBar.css(
+            "width",
+            progress + "%"
+        );
+
+        if ( progress == 100 ) {
+
+            // Show upload result
+            uploadAlert.show();
+
+            // Reset progress bar
+            // Hide upload result
+            setTimeout( function() {
+                progressBar.css( "width", 0 );
+                uploadAlert.hide();
+            }, 3000 );
+
+        }
+    };
+
+    var onUploadComplete = function( data ) {
+
+        var result = data.result;
+        var uploadSuccess = result.status === "ok";
+
+        // List uploaded file
+        data.context = $( "<tr/>" );
+        $.each( data.files, function ( index, file ) {
+            var node = $( "<td />" ).html([
+                "<span>",
+                    file.name,
+                "</span>",
+                "&nbsp;&nbsp;",
+                '<span ',
+                uploadSuccess ? 'class="alert-success" role="alert">' : 'class="alert-danger" role="alert">',
+                    result.message,
+                "</span>"
+            ].join( "" ));
+            node.appendTo( data.context );
+        });
+
+        // Update upload status
+        if ( uploadSuccess ) {
+            uploadStatusData.success++;
+            data.context.appendTo( "#upload-success .result" );
+        } else {
+            uploadStatusData.error++;
+            data.context.appendTo( "#upload-error .result" );
+        }
+
+        uploadSuccessStatus.html( "Uploaded: " + uploadStatusData.success );
+        uploadErrorStatus.html( "Failed: " + uploadStatusData.error );
+
+        // Update missing payslip button, date, and list
+        failedToUploadDate = result.filename.split( " " )[ 0 ];
+        missingPayslipDate.html( failedToUploadDate ).show();
+        missingPayslipButton.removeAttr( "disabled", "" );
+    };
+
+    var initUploadResult = function() {
+
+        // Reset upload status and result
+        uploadSuccessStatus.html( "" );
+        uploadSuccessResult.html( "" );
+        uploadErrorStatus.html( "" );
+        uploadErrorResult.html( "" );
+        uploadStatusData = {
+            "total": 0,
+            "success": 0,
+            "error": 0
+        };
+
+        // Show upload result
+        uploadSuccessContainer.show();
+        uploadErrorContainer.show();
+
+    };
+
+    var initMissingPayslipsResult = function() {
+
+        // Reset missing payslip button and date
+        failedToUploadDate = "";
+        missingPayslipDate.html( failedToUploadDate ).hide();
+        missingPayslipButton.attr( "disabled", "disabled" );
+        missingPayslipListContainer.hide();
+
+    };
 
     $( "#fileupload" ).fileupload({
         url: url,
@@ -30,71 +187,22 @@ $(function () {
 
             xhr.setRequestHeader( "X-CSRFToken", csrfToken );
 
-            // Reset upload status and result
-            uploadSuccessStatus.html( "" );
-            uploadSuccessResult.html( "" );
-            uploadErrorStatus.html( "" );
-            uploadErrorResult.html( "" );
-            uploadStatusData = {
-                "total": 0,
-                "success": 0,
-                "error": 0
-            };
+            initUploadResult();
 
-            // Show upload result
-            uploadSuccessContainer.show();
-            uploadErrorContainer.show();
+            initMissingPayslipsResult();
 
         },
         done: function ( e, data ) {
 
-            console.log( "done:", data );
-            var uploadSuccess = data.result.status === "ok";
-
-            // List uploaded file
-            data.context = $( "<tr/>" );
-            $.each( data.files, function ( index, file ) {
-                var node = $( "<td/>" ).html([
-                    "<span>",
-                        file.name,
-                    "</span>"
-                ].join( "" ));
-                node.appendTo( data.context );
-            });
-            data.context.appendTo( "#upload-success .result" );
-
-            // Update upload status
-            if ( uploadSuccess ) {
-                uploadStatusData.success++;
-            } else {
-                uploadStatusData.error++;
-            }
-
-            uploadSuccessStatus.html( "Uploaded: " + uploadStatusData.success );
-            uploadErrorStatus.html( "Failed: " + uploadStatusData.error );
+            onUploadComplete( data );
 
         },
         progressall: function ( e, data ) {
 
             var progress = parseInt( data.loaded / data.total * 100, 10 );
 
-            progressBar.css(
-                "width",
-                progress + "%"
-            );
+            updateProgressBar( progress );
 
-            if ( progress == 100 ) {
-
-                // Show upload result
-                uploadAlert.show();
-
-                // Reset progress bar
-                // Hide upload result
-                setTimeout( function() {
-                    progressBar.css( "width", 0 );
-                    uploadAlert.hide();
-                }, 3000 );
-            }
         }
     }).prop( "disabled", !$.support.fileInput )
         .parent().addClass( $.support.fileInput ? undefined : "disabled" );
